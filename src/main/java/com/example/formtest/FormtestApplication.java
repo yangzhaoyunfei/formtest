c8ace26066b43bdcb60fdb5a3097cdbac1a055a7
tangzhongwei <tangzw@zjbdos.com>
2018-05-14 19:24:43 +0800

    first commit

---
+++ src/main/java/com/example/formtest/FormtestApplication.java
@@ -0,0 +1,12 @@
+package com.example.formtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormtestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/formtest/FormtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.formtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormtestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
